26cc47c9039687895e1b67f5557ca896165532c4
DELL <varin.mehta22@spit.ac.in>
2024-10-01 09:29:59 +0530

    This is my first commit

---
+++ file.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+//fs.writeFileSync("./test.txt", "Hello world")
+
+// const result = fs.readFileSync("./contacts.txt", "utf-8")
+//console.log(result)
+// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(result)
+//     }
+// })
+
+fs.appendFileSync("./test.txt", new Date().getTime().toLocaleString());
