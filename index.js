a4eab1a5013eb3d58998a17bc0dd6bf0d5f8e0e3
DELL <varin.mehta22@spit.ac.in>
2024-10-01 20:29:32 +0530

    commit

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log("New Request received");
-    res.end("Hello from Server");
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    fs.appendFile("log.txt", log, (err, data) => {
+        switch (req.url) {
+            case "/":
+                res.end("HomePage");
+                break;
+            case "/about":
+                res.end("I am Varin Mehta");
+                break;
+            default:
+                res.end("404 Not Found");
+        }
+    });
 });
 
 server.listen(8000, (err, res) => {
