8feba064c7b0745b8bdb9257c70d2e6c8a1617e4
DELL <varin.mehta22@spit.ac.in>
2024-10-01 22:54:18 +0530

    commit

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    if (req.url === "/favicon.ico") return res.end();
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case "/":
                 res.end("HomePage");
                 break;
             case "/about":
-                res.end("I am Varin Mehta");
+                const username = myUrl.query.mname;
+                res.end(`Hi, ${username}`);
                 break;
             default:
                 res.end("404 Not Found");
