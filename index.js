f9686c6599fdeff742bac2951bf13bccb0c83ecc
varinmehta <mehtavarin2004@gmail.com>
2024-10-02 13:40:13 +0530

    Auto-update

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.get("/about", (req, res) => {
     );
 });
 
-app.listen(8080, () => console.log("Server is running on port 8000"));
+app.listen(8000, () => console.log("Server is running on port 8000"));
