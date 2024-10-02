cd3dbb11a816c9b207b5f8a38d5a645fd90481a8
varinmehta <mehtavarin2004@gmail.com>
2024-10-02 13:00:21 +0530

    Auto-update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const express = require("express");
 
+const app = express();
+app.get("/", (req, res) => {
+    return res.send("Hello from Home page!");
+});
+
+app.get("/about", (req, res) => {
+    return res.send("Hello from About page!");
+});
 const server = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
